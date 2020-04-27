327b566523fe439db0375df2dfc24b3420d43988
Jaiganesh2001 <63258952+Jaiganesh2001@users.noreply.github.com>
2020-04-27 13:47:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var i;
+
+var arr=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+
+
+for(i=0;i<20;i++){
+
+var buttons = document.getElementsByClassName('btn')[i];
+buttons.innerHTML = arr[i];
+
+}
+
+
+    
+
+     
+
+
+
+
+
+    }
+       
+    
+      
